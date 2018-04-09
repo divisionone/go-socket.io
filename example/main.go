a65e0fa680299827f0b0bc7fc31dcf33f38f1b17
Shannon Wynter <me@example.com>
2018-04-09 16:46:12 +1000

    STDLIB - and other customisations

--- example/main.go
+++ example/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/googollee/go-socket.io"
+	"github.com/divisionone/go-socket.io"
 )
 
 func main() {
--- handler_test.go
+++ handler_test.go
@@ -6,7 +6,7 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 	"net/http"
 	"io"
-	"github.com/googollee/go-engine.io"
+	"github.com/divisionone/go-engine.io"
 )
 
 type FakeBroadcastAdaptor struct {}
